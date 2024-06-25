325c73dea0e872bc19b8ffbaca190113c459d89e
Alisha Shaik <136566998+alisha8464@users.noreply.github.com>
2024-06-25 14:06:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const dispaly=document.querySelector(".display")
+const buttons=document.querySelectorAll("button")
+const specialCharacters=["%", "*", "/", "+", "-", "="]
+let output="";
+//console.log(dispaly,buttons)
+
+const calculate = (btnValue)=>{
+    //console.log(btnValue)
+    if (btnValue === "=" && output !== ""){
+        output=eval(output.replace("%","/100"))
+    } else if(btnValue === "AC"){
+        output=""
+    } else if (btnValue === "DEL"){
+        output=output.toString().slice(0,-1)
+    } else {
+        if (output === "" && specialCharacters.includes(btnValue)) return;
+        output+=btnValue
+    }
+    dispaly.value=output
+
+
+    
+}
+
+buttons.forEach((button) => {
+    button.addEventListener("click", (e) => calculate(e.target.dataset.value))
+})
